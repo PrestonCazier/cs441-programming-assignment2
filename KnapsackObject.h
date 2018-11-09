ba8b192b7678e17cca83ed8477129c95396b961b
Preston <cpreston@pdx.edu>
2018-11-08 17:49:08 -0800

    11082018

---
+++ KnapsackObject.h
@@ -0,0 +1,19 @@
+#include <string>
+
+using namespace std;
+
+#ifndef KNAPSACKOBJECT_08112018
+#define KNAPSACKOBJECT_08112018
+
+class KnapsackObject
+{
+	private:
+	string name;
+	int weight;
+	int value;
+
+	public:
+	KnapsackObject();
+	~KnapsackObject();
+};
+#endif KNAPSACKOBJECT_08112018
